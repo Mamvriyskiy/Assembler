b63b3779ef7373e9cad05c3c940891357af4344c
Mamvriyskiy <68384944+Mamvriyskiy@users.noreply.github.com>
2023-02-19 14:24:43 +0300

    add lab_01

---
+++ lab_01/hello.asm
@@ -0,0 +1,23 @@
+.MODEL TINY 
+.DOSSEG 
+.DATA
+    MSG DB "Hello, World!", 0Dh, 0Ah, '$' 
+.CODE
+.STARTUP
+    MOV AH, 09h
+    MOV DX, OFFSET MSG 
+    INT 21h
+    MOV AH, 4Ch
+    INT 21h
+END
+
+
+
+.MODEL TINY .DOSSEG .DATA
+MSG DB "Hello, World!", 0Dh, 0Ah, '$' .CODE
+.STARTUP
+MOV AH, 09h
+MOV DX, OFFSET MSG INT 21h
+MOV AH, 4Ch
+INT 21h
+END
