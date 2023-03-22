80bcbb1d6f2655708c8b9caff3aea27849821f35
Mamvriyskiy <68384944+Mamvriyskiy@users.noreply.github.com>
2023-03-22 16:55:43 +0300

    complete lab_04

--- lab_04/main.asm
+++ lab_04/main.asm
@@ -1,20 +1,21 @@
 SD SEGMENT PARA STACK 'STACK'
-    db 100 dup(0)
+    db 256 dup(0)
 SD ENDS
 
 DATA SEGMENT PARA PUBLIC 'DATA'
     matrix db 81 dup(0)
 
-    input_n     DB  'Input n: '
+    input_n     DB  'Input columns: '
                 DB '$'
-    n db 1
+    n db 0
 
-    input_m     DB  'Input m: '
+    input_m     DB  'Input rows: '
                 DB '$'
-    m db 1
+    m db 0
 
-    size db 1
-
+    sum db 0
+    minsum db 1
+    minindex db 0
 
 DATA ENDS
 
@@ -24,28 +25,143 @@ SC1 SEGMENT PARA PUBLIC 'CODE'
 input_num:
     mov ah, 01h
     int 21h
-    sub al, '0'
-    ret
-
-size_matrix:
-    mov ax, 0h
-    mov cx, 0h
-    mov al, m
-    mul n
-    mov cx, ax
+    sub AL, '0'
     ret
 
 create_matrix:
-    call size_matrix
-	MOV BX, 0h
+    mov si, 0 //по строкам
+    mov cl, [m]
+    label1:
+        mov di, 0
+        add di, si //по строке
+        mov dh, cl
+        mov cl, [n]
 
-input_matrix:
-    mov ah, 01h
-    int 21h
-    mov matrix[bx], al
-    int 21h
-    loop input_matrix
+        label2:
+            mov ah, 01h
+            int 21h
+            mov matrix[di], al
+            int 21h
+            inc di
+        loop label2
+
+        add si, 9
+        mov cl, dh
+    loop label1
+
     ret
+
+print:
+    mov ah, 02h
+    mov dl, 10
+    int 21h
+    mov dl, 13
+    int 21h
+    ret
+
+output_matrix:
+    call print
+
+    xor si, si
+    mov cl, [m]
+
+    lab1:
+        xor di, di
+        add di, si
+        mov dh, cl
+        mov cl, [n]
+        mov ah, 2
+
+        lab2:
+            mov dl, matrix[di]
+            int 21h
+            mov dl, ' '
+            int 21h
+            inc di
+        loop lab2
+
+        call print
+
+        mov cl, dh
+        add si, 9
+    loop lab1
+
+    ret
+
+
+delete_min_sum_column:
+    call search_min_sum
+    
+    call delete_column
+
+    ret
+
+search_min_sum:
+    mov si, 0
+    mov cl, [n]
+    lable1:
+        mov ah, 0
+        mov di, si
+        mov dh, cl
+        mov cl, [m]
+        lable2:
+            mov bl, matrix[di]
+            sub bl, 30h
+            add ah, bl
+            
+            add di, 9
+        loop lable2
+
+        cmp dh, [n]
+		jne furher ;не равны
+		jmp set_min_sum
+		furher:
+		cmp ah, [minsum]
+		jb set_min_sum ;если меньше
+		jae endofif
+		set_min_sum:
+			xchg ah, [minsum]
+			xor ax, ax
+			add ax, si
+			xchg [minindex], al
+		endofif:
+
+        inc si
+        mov cl, dh
+    loop lable1
+
+    ret
+
+delete_column:
+	mov cx, 0 
+	mov di, 0 
+	mov si, 0 
+	mov cl, [m] ;иниц количестом строк
+
+run_lines:
+	mov dh, cl
+	mov cl, [n] ;иниц количеством столбцов
+	mov ax, 0
+    mov al, [minindex]
+	sub cl, al
+	mov di, ax
+	add di, si
+	
+	shift:
+		mov ah, 0
+		xchg ah, matrix[di + 1]
+		mov byte ptr matrix[di], ah
+		inc di
+	loop shift
+
+	add si, 9
+	mov cl, dh
+	loop run_lines
+	
+	;mov ah, n
+	dec n
+	;xchg ah, n
+	ret
 
 main:
     mov ax, DATA
@@ -69,7 +185,15 @@ main:
     mov m, al
     int 21h
 
-    call create_matrix
+    call create_matrix ;создание матрицы
+
+    call output_matrix ;вывод матрицы
+
+    call delete_min_sum_column
+
+    call output_matrix
+
+    mov dl, n
 
     mov ax, 4c00h
     int 21h
